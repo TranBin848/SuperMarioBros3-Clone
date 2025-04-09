07fe0617ad15d94e5995a24b6ea6a604c2d802fd
TranBin848 <tranbinn848@gmail.com>
2025-04-10 00:05:35 +0700

    Init project with basic textures, sprites and word 1-1 draft

---
+++ Main/Scene.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "KeyEventHandler.h"
+#include <string> 
+using namespace std;
+/*
+*  Abstract class for a game scene
+*/
+class CScene
+{
+protected:
+	LPKEYEVENTHANDLER key_handler;
+	int id;
+	wstring sceneFilePath;
+
+public: 
+	CScene(int id, const wstring& filePath)
+	{
+		this->id = id;
+		this->sceneFilePath = filePath;
+		this->key_handler = NULL;
+	}
+
+	LPKEYEVENTHANDLER GetKeyEventHandler() { return key_handler; }
+	virtual void Load() = 0;
+	virtual void Unload() = 0;
+	virtual void Update(DWORD dt) = 0;
+	virtual void Render() = 0; 
+};
+typedef CScene * LPSCENE;
+
+
+class CSceneKeyHandler : public CKeyEventHandler
+{
+protected: 
+	CScene * scene; 
+
+public:
+	virtual void KeyState(BYTE *states) = 0;
+	virtual void OnKeyDown(int KeyCode) = 0;
+	virtual void OnKeyUp(int KeyCode) = 0;
+	CSceneKeyHandler(LPSCENE s) :CKeyEventHandler() { scene = s; }
+};
+
+typedef CSceneKeyHandler* LPSCENEKEYHANDLER;
